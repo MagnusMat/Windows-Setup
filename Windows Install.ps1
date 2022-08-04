45595f5c09197a96dac4bc2fd1716dbdfdb05e99
Magnus Mathiesen <magnus_mat@pm.me>
2022-08-04 14:38:58 +0200

    Fixed fonts

--- Windows Install.ps1
+++ Windows Install.ps1
@@ -278,7 +278,7 @@ Remove-Item FiraCode, FiraMono
 Set-Location Fonts
 
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
-foreach ($file in Get-ChildItem "Fira *.ttf") {
+foreach ($file in Get-ChildItem "Fira*.ttf") {
     $fileName = $file.Name
     if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
         Write-Output $fileName
