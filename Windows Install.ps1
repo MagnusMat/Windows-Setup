6c319746ef5c5b499f111ac5ce80b5dd2cfabdef
Magnus Mathiesen <magnus_mat@pm.me>
2022-09-22 00:28:58 +0200

    Update Windows Install.ps1

--- Windows Install.ps1
+++ Windows Install.ps1
@@ -132,10 +132,6 @@ function Install-GitHub {
     Remove-Item "$Name.$FileType"
 }
 
-# -------------------- Wallpaper --------------------
-
-Invoke-WebRequest https://cdn.wallpaperhub.app/cloudcache/d/3/c/2/b/f/d3c2bf863b952ad8d93816729ce85bb0bbebcbc8.png -OutFile Wallpaper.png
-
 # -------------------- Confirmations --------------------
 
 # Install Laptop Desktop Prompt
