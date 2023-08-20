876da45daefbc11554e9a0077a6c371c2cc57d2e
Magnus Mathiesen <magnus_mat@pm.me>
2023-08-20 13:20:07 +0200

    Fixed profile

--- Windows Install.ps1
+++ Windows Install.ps1
@@ -247,9 +247,8 @@ gh repo clone MagnusMat/PowerShell-Scripts
 Set-Location ~
 
 # Set PowerShell Profile
-Invoke-WebRequest -Uri https://github.com/MagnusMat/Windows-Setup/ -OutFile ./Microsoft.PowerShell_profile.ps1
-Copy-Item ./Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-Move-Item ./Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item $env:USERPROFILE/GitHub/PowerShell-Scripts/Profile/Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Move-Item $env:USERPROFILE/GitHub/PowerShell-Scripts/Profile/Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
 Start-Process -FilePath pwsh.exe -ArgumentList {
     # Execution Permission
