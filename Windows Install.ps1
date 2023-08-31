ca28a4cd250adbe089605aea35227c53332054f5
Magnus Mathiesen <magnus_mat@pm.me>
2023-08-31 13:54:32 +0200

    Update Windows Install.ps1

--- Windows Install.ps1
+++ Windows Install.ps1
@@ -616,6 +616,7 @@ $wingets += Add-Winget -Name "Obsidian" -ID "Obsidian.Obsidian"
 
 # Oh My Posh
 $wingets += Add-Winget -Name "Oh My Posh" -ID JanDeDobbeleer.OhMyPosh -Source "winget"
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/m365princess.omp.json" | Invoke-Expression
 
 # Onion Share
 $OnionShareParams = @{
